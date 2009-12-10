16e3a9508147bb8a0159aa64fa29383dc9e93488
Jan Burkhardt <jan.burkhardt@freiheit.com>
2009-12-10 22:18:54 +0100

    problem 0001 solved

---
+++ src/de/bjrke/euler/problem0001/Problem0001.scala
@@ -0,0 +1,17 @@
+package de.bjrke.euler.problem0001;
+
+object Problem0001 {
+  def main(args : Array[String]) : Unit = {
+    val result = (0 to 999).toList.filter( isMultipleOf3Or5(_)).foldLeft(0) { _ + _ }
+    println(result)
+  }
+  
+  private def isMultipleOf3Or5( a: Int ) : Boolean = {
+    isMultipleOf(a,3) || isMultipleOf(a,5)
+  }
+
+  private def isMultipleOf( a: Int, b: Int) : Boolean = {
+    a % b == 0
+  }
+  
+}
