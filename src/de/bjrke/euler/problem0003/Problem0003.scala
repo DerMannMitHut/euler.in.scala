01ad1e39ae7edc4d5d6638a5a2db37297ace768c
Jan Burkhardt <jan.burkhardt@freiheit.com>
2009-12-10 22:58:26 +0100

    problem 3 solved: result 6857

---
+++ src/de/bjrke/euler/problem0003/Problem0003.scala
@@ -0,0 +1,20 @@
+package de.bjrke.euler.problem0003
+
+import de.bjrke.euler.problem0007.Problem0007
+
+object Problem0003 {
+  def main(args : Array[String]) : Unit = {
+    println(largestPrimeFactor(600851475143L, List( 3, 2 )))    
+  }
+
+  def largestPrimeFactor(x : Long, primes : List[Long] ) : Long = {
+    if ( x == primes.head ) {
+      primes.head
+    } else if ( x % primes.head == 0 ) {
+      largestPrimeFactor(x/primes.head, primes)
+    } else {
+      largestPrimeFactor(x, Problem0007.nextPrime(primes) :: primes )
+    }
+  }
+
+}
--- src/de/bjrke/euler/problem0007/Problem0007.scala
+++ src/de/bjrke/euler/problem0007/Problem0007.scala
@@ -2,19 +2,19 @@ package de.bjrke.euler.problem0007
 
 object Problem0007 {
   def main(args : Array[String]) : Unit = {
-    val a: List[Int] = List( 3, 2)
+    val a: List[Long] = List( 3, 2)
     println(findPrime(10001,a))
   }  
   
-  def findPrime(number: Int, primes: List[Int]) : Int = {
+  def findPrime(number: Long, primes: List[Long]) : Long = {
     if ( primes.size == number ) {
       primes.head
     } else {
       findPrime(number, nextPrime(primes) :: primes)
     }
   }
-  def nextPrime( previous: List[Int] ) : Int = nextPrime( previous, previous.head )
-  def nextPrime( previous: List[Int], lastTested: Int ) : Int = {
+  def nextPrime( previous: List[Long] ) : Long = nextPrime( previous, previous.head )
+  def nextPrime( previous: List[Long], lastTested: Long ) : Long = {
     val cand = lastTested + 2
     if ( previous.exists ( cand % _ == 0 ) ) {
       nextPrime( previous, cand )
